ae2dd93f1cd64d24089f548d78514831d7931e0c
Ben Wiklund <ben@daisyowl.com>
2014-06-12 20:13:51 -0700

    i guess this is what i'll be making

--- popup.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener( function(tab){
-  console.log("An occurrence is transpiring!");
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  })
-})
